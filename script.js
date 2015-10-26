97610708882dbea7eb9738e07ca25a7e9af02dfa
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-10-26 12:45:27 -0400

    Hide all divs when match

--- script.js
+++ script.js
@@ -4,79 +4,116 @@ $(document).ready(function(){
   var imgNumber;
   var saveImage = $("." + imgNumber);
   var imgClass;
+  var image;
+
   $("#start").on("click", function(){
     $(".square").css("display", "inline-block");
   });
 
   $(".square").on("click", function(){
     if (click === 0){
-    click++;
-    console.log(click);
-    imgNumber = $(this).attr("id");
-      assignImage(randomImage());
-      $(this).css("opacity", 1.0);
-      $(this).addClass("active");
+      click++;
+      imgNumber = $(this).attr("id");
+      checkForCurrentImage();
+      $("." + imgNumber).addClass("active");
     }
-    else if(click > 0 ){
-    click++;
-    console.log(click);
-    imgNumber = $(this).attr("id");
-    assignImage(randomImage());
-    $(this).css("opacity", 1.0);
-    $(this).addClass("active");
+    else if(click === 1 ){
+      imgNumber = $(this).attr("id");
+      checkForCurrentImage();
+      $("." + imgNumber).addClass("active2");
+      click++;
+    }
+    else if (click === 2){
+      checkForMatch();
+      click = 0;
     }
   });
 
-  function assignImage(picture){
+  function checkForCurrentImage(){
+    if($("." + imgNumber).attr("src") !== ""){
+      $("." + imgNumber).css("visibility", "visible");
+    }
+    else{
+      assignImage();
+      $("." + imgNumber).css("visibility", "visible");
+    }
+  }
+
+  function checkForMatch(){
+      if($(".active").attr("src") === $(".active2").attr("src")){
+        alert("IT'S A MATCH!!!");
+        $(".active").parent().hide();
+        $(".active2").parent().hide();
+        $(".active").removeClass("active");
+        $(".active2").removeClass("active2");
+      }
+      else{
+        alert("IT'S NOT A MATCH!!");
+        $(".active").css("visibility", "hidden");
+        $(".active2").css("visibility", "hidden");
+        $(".active").removeClass("active");
+        $(".active2").removeClass("active2");
+
+      }
+  }
+
+  function assignImage(){
+    var picture = randomImage();
     if (picture === "boo"){
       imgClass = "boo";
-      console.log(imgClass + " TEST" );
-      // if($("." + imgClass).length < 2){
-        $("." + imgNumber).attr("src", "images/boo.jpg");
-        $("#" +imgNumber).addClass("boo");
-      // }
+      console.log(imgClass + "test!");
+      image = "images/boo.jpg";
+      // must have setAttributeForImage last!
+      setAttributeForImage();
     }
     else if(picture === "witch"){
       imgClass = "witch";
-      console.log(imgClass + " TEST" );
-      // if($("." + imgClass).length < 2){
-        $("." + imgNumber).attr("src", "images/witch1.jpg");
-        $("#" +imgNumber).addClass("witch");
-      // }
+      console.log(imgClass + "test!");
+      image = "images/witch1.jpg";
+      // must have setAttributeForImage last!
+      setAttributeForImage();
     }
     else if(picture === "dude"){
       imgClass = "dude";
-      console.log(imgClass + " TEST" );
-      // if($("." + imgClass).length < 2){
-      $("." + imgNumber).attr("src", "images/dude.jpg");
-      $("#" +imgNumber).addClass("dude");
-      // }
+      console.log(imgClass + "test!");
+      image = "images/dude.jpg";
+      // must have setAttributeForImage last!
+      setAttributeForImage();
     }
     else if(picture === "zombie"){
       imgClass = "zombie";
-      console.log(imgClass + " TEST" );
-      // if($("." + imgClass).length < 2){
-        $("." + imgNumber).attr("src", "images/zombie.jpg");
-        $("#" +imgNumber).addClass("zombie");
-      // }
+      console.log(imgClass + "test!");
+      image = "images/zombie.jpg";
+      // must have setAttributeForImage last!
+      setAttributeForImage();
     }
     else if(picture === "creepy"){
       imgClass = "creepy";
-      console.log(imgClass + " TEST" );
-      // if($("." + imgClass).length < 2){
-        $("." + imgNumber).attr("src", "images/creepy.jpg");
-        $("#" +imgNumber).addClass("creepy");
-      // }
+      console.log(imgClass + "test!");
+      image = "images/creepy.jpg";
+      // must have setAttributeForImage last!
+      setAttributeForImage();
     }
     else if(picture === "gargoyle"){
       imgClass = "gargoyle";
-      console.log(imgClass + " TEST" );
-      // if($("." + imgClass).length < 2){
-        $("." + imgNumber).attr("src", "images/gargoyle.jpg");
-        $("#" +imgNumber).addClass("gargoyle");
-      // }
+      console.log(imgClass + "test!");
+      image = "images/gargoyle.jpg";
+      // must have setAttributeForImage last!
+      setAttributeForImage();
     }
   }
+
+  function setAttributeForImage(){
+    if($("." + imgClass).length < 2){
+      $("." + imgNumber).attr("src", image);
+      $("." +imgNumber).addClass(imgClass);
+      console.log("added a class of " + imgClass);
+    }
+    else {
+      assignImage();
+    }
+  }
+
   function randomImage() {
     var randomNumber = Math.random();
     if (randomNumber < 0.16) {
